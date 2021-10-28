a5c9912e4911b3ee169015f98302a7f6eef155da
shdxw <56305521+shdxw@users.noreply.github.com>
2021-10-29 00:24:02 +1000

    init spring + routes

---
+++ src/main/java/com/example/fileserver/FileServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.fileserver;
+
+import com.example.fileserver.property.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
+public class FileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServerApplication.class, args);
+    }
+
+}
